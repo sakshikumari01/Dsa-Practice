1e847609e1d8523baf7712d4bdfa934710057fed
Sakshi Kumari <mishra0108sakshi@gmail.com>
2026-07-13 19:59:34 +0530

    Time: 2 ms (98.63%), Space: 43.7 MB (88.78%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder ans = new StringBuilder();
+
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += num1.charAt(i) - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += num2.charAt(j) - '0';
+                j--;
+            }
+
+            ans.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return ans.reverse().toString();
+    }
+}
